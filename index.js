cab668c4ffcb0c5e8cb96eaec55a902f02fdd191
noralund <125641002+noralund@users.noreply.github.com>
2023-05-09 10:12:21 +0200

    .

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
+// const callback = (entries, observer) => {
+//     entries.forEach(entry => {
+//       if (entry.isIntersecting) {
+//         document.body.style.backgroundColor = entry.target.dataset.color;
+//       }
+//     });
+//   };
+  
+//   const changes = document.querySelectorAll('.change');
+  
+//   const observer = new IntersectionObserver(callback, { threshold: .1 });
+  
+//   changes.forEach(change => {
+//     observer.observe(change);
+//   });
+
 const callback = (entries, observer) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        document.body.style.backgroundColor = entry.target.dataset.color;
-      }
-    });
-  };
-  
-  const changes = document.querySelectorAll('.change');
-  
-  const observer = new IntersectionObserver(callback, { threshold: .1 });
-  
-  changes.forEach(change => {
-    observer.observe(change);
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      document.body.style.backgroundColor = entry.target.dataset.color;
+    }
   });
+};
+
+const changes = document.querySelectorAll('.change');
+const fish = document.querySelectorAll('.fish');
+
+const observer = new IntersectionObserver(callback, { threshold: .1 });
+
+changes.forEach(change => {
+  observer.observe(change);
+});
+
+fish.forEach(fishElem => {
+  observer.observe(fishElem);
+});
