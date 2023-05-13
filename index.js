bc44dfaea99d7e4460bf0ea0185314d6103e1df8
noralund <125641002+noralund@users.noreply.github.com>
2023-05-14 01:16:53 +0200

    read me file

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-// const callback = (entries, observer) => {
-//     entries.forEach(entry => {
-//       if (entry.isIntersecting) {
-//         document.body.style.backgroundColor = entry.target.dataset.color;
-//       }
-//     });
-//   };
-  
-//   const changes = document.querySelectorAll('.change');
-  
-//   const observer = new IntersectionObserver(callback, { threshold: .1 });
-  
-//   changes.forEach(change => {
-//     observer.observe(change);
-//   });
 
 const callback = (entries, observer) => {
   entries.forEach(entry => {
